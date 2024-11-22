4476d2526990c2777886e32c327d9faa3429e4f0
ruiyao <1152014767@qq.com>
2024-11-22 20:50:03 +0800

    wip

---
+++ src/controls.rs
@@ -0,0 +1,3 @@
+pub struct Controls {
+  
+}
--- src/slider.rs
+++ src/slider.rs
@@ -1,15 +1,14 @@
-use eframe::egui::{emath::Numeric, vec2, Color32, DragValue, Label, Layout, Rect, Sense, Widget};
+use eframe::egui::{emath::Numeric, vec2, Color32, DragValue, Layout, Rect, Sense, Widget};
 
 pub struct Slider<'a, T> {
-  pub tint: &'a str,
   pub title: Option<&'a str>,
   pub range: std::ops::RangeInclusive<T>,
   pub get_set_value: Box<dyn 'a + FnMut(Option<T>) -> T>,
 }
 
 impl<'a, T> Slider<'a, T> {
-  pub fn from_get_set(range: std::ops::RangeInclusive<T>, tint: &'a str, get_set_value: impl 'a + FnMut(Option<T>) -> T) -> Self {
-    Self { tint: &tint, range, get_set_value: Box::new(get_set_value), title: None }
+  pub fn from_get_set(range: std::ops::RangeInclusive<T>, get_set_value: impl 'a + FnMut(Option<T>) -> T) -> Self {
+    Self { range, get_set_value: Box::new(get_set_value), title: None }
   }
 
   pub fn with_title(mut self, title: &'a str) -> Self {
@@ -56,7 +55,8 @@ impl<'a, T: Numeric> Widget for Slider<'a, T> {
       } else {
         if ui.visuals().dark_mode { "01" } else { "06" }
       };
-      painter.rect_filled(rect, 4.0, Color32::from_hex(format!("#{}{}", self.tint, background_alpha).as_str()).unwrap());
+      let tint = if ui.visuals().dark_mode { "FFFFFF" } else { "000000" };
+      painter.rect_filled(rect, 4.0, Color32::from_hex(format!("#{}{}", tint, background_alpha).as_str()).unwrap());
 
       if let Some(title) = self.title {
         ui.allocate_ui_with_layout(vec2(100.0, 20.0), Layout::left_to_right(eframe::egui::Align::Center), |ui| {
@@ -64,23 +64,21 @@ impl<'a, T: Numeric> Widget for Slider<'a, T> {
         });
       }
       // 2. draw the cursor
-      // if response.hovered() {
-        let cursor_size = vec2(4.0, size.y);
-        let start = self.range.start().to_f64();
-        let end = self.range.end().to_f64();
-        let v = value.to_f64();
-        let mut per = (v - start) / (end - start);
-        
-        per = per.min(1.0).max(0.0);
-        let pos = rect.min + vec2((per as f32) * (size.x - cursor_size.x), 0.0);
-        let cursor_rect = Rect { min: pos, max: pos + cursor_size };
-        let cursor_alpha = if response.hovered() { 
-          if ui.visuals().dark_mode { "FF" } else { "CC" }
-        } else {
-          if ui.visuals().dark_mode { "0A" } else { "18" }
-        };
-        painter.rect_filled(cursor_rect, 4.0, Color32::from_hex(format!("#{}{}", self.tint, cursor_alpha).as_str()).unwrap());
-      // }
+      let cursor_size = vec2(4.0, size.y);
+      let start = self.range.start().to_f64();
+      let end = self.range.end().to_f64();
+      let v = value.to_f64();
+      let mut per = (v - start) / (end - start);
+      
+      per = per.min(1.0).max(0.0);
+      let pos = rect.min + vec2((per as f32) * (size.x - cursor_size.x), 0.0);
+      let cursor_rect = Rect { min: pos, max: pos + cursor_size };
+      let cursor_alpha = if response.hovered() { 
+        if ui.visuals().dark_mode { "FF" } else { "CC" }
+      } else {
+        if ui.visuals().dark_mode { "0A" } else { "18" }
+      };
+      painter.rect_filled(cursor_rect, 4.0, Color32::from_hex(format!("#{}{}", tint, cursor_alpha).as_str()).unwrap());
 
       if response.is_pointer_button_down_on() {
         let mut per = (response.interact_pointer_pos().unwrap().x - rect.min.x) / size.x;
